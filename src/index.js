d8d0691552ef563c22d6ae0c6616b40511ca65b6
aiyaya <1045858991@qq.com>
2023-03-14 15:36:55 +0800

    写一个react列表

--- src/index.js
+++ src/index.js
@@ -1,41 +1,61 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 引入样式表
+import './index.css'
 
-// 创建一个按钮
-// const btn = <button
-//   onClick={changeList}
-// >
-//   点击修改
-// </button>;
+// 创建一个react元素
+const App = <div className="logs">
+    <div className="item">
+       {/* 日期的容器 */}
+        <div className="date">
+          <div className="month">
+            三月
+          </div>
+          <div className="day">
+            14
+          </div>
+        </div>
+        {/* 日志内容的容器 */}
+        <div className="content">
+          <h2 className="desc">学习react</h2>
+          <div className="time">40分钟</div>
+        </div>
+    </div>
+    <div className="item">
+       {/* 日期的容器 */}
+        <div className="date">
+          <div className="month">
+            三月
+          </div>
+          <div className="day">
+            14
+          </div>
+        </div>
+        {/* 日志内容的容器 */}
+        <div className="content">
+          <h2 className="desc">学习react</h2>
+          <div className="time">40分钟</div>
+        </div>
+    </div>
+    <div className="item">
+       {/* 日期的容器 */}
+        <div className="date">
+          <div className="month">
+            三月
+          </div>
+          <div className="day">
+            14
+          </div>
+        </div>
+        {/* 日志内容的容器 */}
+        <div className="content">
+          <h2 className="desc">学习react</h2>
+          <div className="time">40分钟</div>
+        </div>
+    </div>
+  </div>
 
-
-let list = ['苹果', '香蕉', '橘子'];
-
-let listArr = <ul>
-  {list.map(item => <li key={item}>{item}</li>)}
-</ul>
-
-document.getElementById('btn').onclick = () => {
-  list = [ '菠萝', '苹果', '香蕉', '橘子'];
-  listArr = <ul>
-    {list.map(item => <li key={item}>{item}</li>)}
-  </ul>;
-  root.render(listArr);
-};
-
-
-
+// 获取根元素
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(listArr);
-
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 渲染
+root.render(App)
